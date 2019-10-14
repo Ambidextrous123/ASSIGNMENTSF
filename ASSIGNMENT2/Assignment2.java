cdb90d5b5698d18e9a5024d8dfa3282ab269bd7f
EC2 Default User <ec2-user@ip-172-31-43-105.us-east-2.compute.internal>
2019-10-14 21:45:24 +0000

    my first assignment freshman

---
+++ ASSIGNMENT2/Assignment2.java
@@ -0,0 +1,9 @@
+ class Assignment2 {
+    public static void main (String args[]){
+    
+         
+
+        System.out.print("Name: Lucky Airaodion\nMajor: Computer Science");
+        System.out.print("\nYear: Sophomore\n");
+        }
+     }
