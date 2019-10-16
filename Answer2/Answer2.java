ee40505ff22d598d6028e3497cce19900ed6fa57
EC2 Default User <ec2-user@ip-172-31-43-105.us-east-2.compute.internal>
2019-10-16 02:12:17 +0000

    my practice question answer

--- Answer2/Answer2.java
+++ Answer2/Answer2.java
@@ -1,23 +1,19 @@
 import java .util.Scanner;
-
-   class Answer {
-
+   class Answer2 {
        public static void main (String args[]){
-
        Scanner sc = new Scanner(System.in);
-       
        System.out.println("enter the mark...");
        int mark = sc.nextInt();
-       
+
 
        if (mark > 80){
         char grade = 'A';
       System.out.println("Grade :" + grade);
-      
+
        }else if (mark <= 80 && mark >= 60){
          char grade = 'B';
        System.out.println("Grade :" + grade);
-           
+
       }else if (mark <= 60 && mark >= 50){
          char grade = 'C';
        System.out.println("Grade :" + grade);
@@ -32,10 +28,6 @@ import java .util.Scanner;
      }else if ( mark < 25){
         char grade = 'F';
        System.out.println("Grade :" + grade);
-
-
-
-       
-       }
-     }
-   }
+         }
+      }
+    }
