f66dedddea0fe9ad2d2e89bd5242a1536e790d1b
EC2 Default User <ec2-user@ip-172-31-43-105.us-east-2.compute.internal>
2019-10-22 23:24:00 +0000

    kattis problem1

---
+++ Kattisproblem/Kattisproblem.java
@@ -0,0 +1,15 @@
+import java .util.*;
+    class Kattisproblem {
+        public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+         System.out.println("enter the integer...");
+         int n = sc.nextInt();
+         int i = 1;
+    
+          while ( i <= n){
+          System.out.println("Abracadabra :" + i);
+                  i++;
+              
+          }
+        }
+    }
