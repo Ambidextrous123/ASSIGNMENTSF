9b130ee775f94283ce58ebf83d917d6ff080a977
EC2 Default User <ec2-user@ip-172-31-43-105.us-east-2.compute.internal>
2019-10-27 13:14:18 +0000

    my programmpractice

---
+++ Counter3/Counter3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+    class Counter3 {
+          public static void main (String args[]){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter the value of a...");
+            int a = sc.nextInt();
+              System.out.println("enter the value of  b...");
+              int b = sc.nextInt();
+       
+             int counter = 0;
+               
+              for(   ; a <= b ;a++){
+              System.out.println("I am from the USA"); 
+          
+               if (a % 2 == 1){
+                   counter++;
+               System.out.println("I am very good");
+
+             
+                  }
+              }
+        }
+      
+}    
+
+
+
