b483afa85e877426566d663f329afe9d33f92ea7
EC2 Default User <ec2-user@ip-172-31-43-105.us-east-2.compute.internal>
2019-11-09 20:03:48 +0000

    ist method

---
+++ Method/Method.java
@@ -0,0 +1,10 @@
+class Method {
+     public static void main(String args[]){
+      sayHelloWorld();
+     
+     }
+
+     static void sayHelloWorld(){
+     System.out.println("Hello world");
+     }
+ } 
