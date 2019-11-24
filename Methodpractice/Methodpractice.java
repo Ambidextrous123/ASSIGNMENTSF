8ef80bcc7fac31a3f6ffcaf4165f24ded3034068
EC2 Default User <ec2-user@ip-172-31-43-105.us-east-2.compute.internal>
2019-11-24 01:48:55 +0000

    Methodpractice

---
+++ Methodpractice/Methodpractice.java
@@ -0,0 +1,382 @@
+import java .util.Scanner;
+class Methodpractice {
+      public static void main (String args[]){
+         //Scanner sc = new Scanner(System.in);
+
+      //     System.out.println( " enter the value a ...");
+        //    System.out.println( "enter the value b...");
+
+          // int k =  avg ( 7, 5);
+         // System.out.println( " Average: " + k);    
+
+
+
+     //    int w = sum( 3 , 5);
+       //  System.out.println("SUM: " + w);
+       
+         
+              //int sum = 0;
+          // int y = sum( 2, 10);
+          // System.out.println("SUM: " + y);
+         
+                                       
+
+      //static int avg(int a , int b){
+        // return (a + b)/2 ;
+
+
+       // static int sum( int x, int y){
+         //   return ( x+y);
+                                
+
+
+        // static int sum( int m,int b ){
+            //int sum = 0;               
+          // for  ( int i = m ;m <= b;m++){
+            //sum = sum + m;
+               
+          
+          //   return sum;
+     
+
+      //     int k = sumOdd( 2, 10);
+        //   System.out.println("SUM:" + k);
+           
+                          
+    
+                    
+              
+
+        // static int sumOdd ( int a , int b){
+           //   int sum = 0;
+             //while( a < b){
+               //  if ( a % 2 == 1){
+               // sum = sum + a;
+            //       }
+              //        a++;
+          //      }
+               
+               // return sum ;             
+                                 
+          
+ 
+     //           getGreeting();
+           
+  
+      
+     //     }    
+     //     static void getGreeting(){
+       //   System.out.println("Greetings humans");
+
+         //    }
+        // }
+   
+          
+           //sayHi();
+    
+
+           
+            //}
+            //static void sayHi(){
+            //System.out.println(	"Hello");
+          // }
+     // }
+     
+           //int y = doubleInt( 8);
+           //System.out.println( "DOUBLE VALUE:" + y);
+           
+ 
+       
+           //}
+
+            //static int doubleInt( int a){
+           // return 2 * a;
+           //}     
+      //}
+ 
+
+
+           //int tripleValue  = tripleInt( 3 );
+           
+           //System.out.println("THE TRIPLE VALUE :" + tripleValue);
+
+
+           //}
+          // static int tripleInt(int arg ){
+            //  return 3 * arg ;
+          // }
+      //}
+
+           
+             //double i =  min ( 2.2,2.2);
+             //System.out.println("MIN:" + i);
+ 
+    
+            //}
+
+           //static double min( double a , double b){
+              //return ( a < b)? a : b ;
+               
+              
+             
+            //boolean result = isOdd( 4 );
+             //System.out.println( "RESULT:" + result);
+             
+            
+           //}   
+            
+          // static boolean isOdd ( int arg){
+            //   return ( arg % 2 == 1)? true  : false ;
+               
+ 
+             //int maxValue = max( 2,5);
+             // System.out.println("MAXVALUE : " + maxValue);
+   
+           // }                         
+         //   static int max ( int a,int b){
+       //       return ( a > b )? a : b;
+     //        }
+   //}
+                //int a = sc.nextInt();
+
+                 //int y  = factorial ( 3);
+                // System.out.println( "FACTORIAL: " + y);
+       
+                 //}       
+                //static int factorial ( int a){
+                  //     int fact = 1;
+                       
+                    //while(  1 <= a){
+                      //  fact = fact *a;
+                     
+                    //        a--;
+                     //}              
+                    //return fact ;  
+                 // }
+            // }
+              
+                  
+                 
+                 // int a = sc.nextInt();
+                  //int b = sc.nextInt();
+                  
+                                         
+                    //int k =   square( a,b);
+                   //System.out.println( "SQUARE MAX:" + k);
+
+
+  
+                //}
+
+                  //static int square( int a, int b){      
+                    // return ( a > b )? a*a : b*b ;
+                  //}
+ 
+               //}
+   
+         
+                
+                  
+                   
+                 // suM();
+                
+              
+               
+               // }       
+
+                 //static void suM( ){
+             //Scanner sc =  new Scanner(System.in);
+              //System.out.println("enter the value of a...");
+                // int a = sc.nextInt();
+              //System.out.println("enter the value of b...");
+                // int b = sc.nextInt();
+                  //  int sum = 0;
+                    //sum = a + b;
+                   //System.out.println("SUM : " + sum);
+                  //}
+
+               //}
+            
+
+              //      int a = sc.nextInt();
+                //    int b = sc.nextInt();
+
+                 //    int result = proDuct( a,b);
+                   //System.out.println("PRODUCT:" + result);
+
+                   //}
+
+                    //static int proDuct ( int a , int b){
+                      //    return a*b;
+                  // }
+             // }
+  
+                      
+                      // circumference();                                  
+                     
+                     
+                     // }        
+
+                     //static void circumference ( ){
+                       //  Scanner sc = new Scanner(System.in);
+                         //System.out.println("enter the value of the radius ...");
+                           //double x = 3.1423;  
+                            //int r = sc.nextInt();
+                         
+                     // System.out.println( "CIRCUMFERNCE : " + (2*x*r));
+                     // System.out.println(" AREA : " + (x*r));
+                       //  }
+                    //}
+                           
+                        
+
+                        //  max();
+                    
+                    //    }
+
+                      //  static void max( ){
+                       //Scanner sc = new Scanner (System.in);
+                       //System.out .println("enter the value of a...");
+                       //int a = sc.nextInt();
+                      //System.out.println("enter the value of b...");
+                       //int b = sc.nextInt();
+                       // System.out.println("enter the value of c...");
+                      //int c = sc.nextInt();
+                           
+                        //if ( a > b && a > c){
+                           
+                       //System.out.println( "MAXIMUM: " + a);
+                         
+                         //  }else if  ( b > a && b > c){
+                           //  System.out.println( "MAXIMUM: " + b);
+                             
+       
+                           //}else if ( c > a && c > b){
+                                                      
+                        //System.out.println( "MAXIMUM: " + c);
+                           
+                        //}   else if ( a == b && c == a){
+                        //System.out.println( "This is the   maximum value ");
+               //}
+           //}
+// }
+                              
+                      // numBer();
+
+
+
+
+                        // }
+
+                        //static void numBer(){
+                      //Scanner sc = new Scanner(System.in);
+                        //System.out.println("enter the value of a...");
+                        //int a  = sc.nextInt();
+                       
+                        //if ( a % 2 == 1){
+                          //String str1 = "odd";
+                     //System.out.println("NUMBER: " + str1);
+                          
+             
+                       // }else {
+                         //     String str2 = "even";
+                      //System.out.println( "NUMBER : " + str2);
+                     //}
+                //}
+          //}
+                  
+
+                  
+                   
+                    // yEar();	
+  
+     
+                      
+
+
+
+                  //}
+
+                 //static void  yEar( ){
+
+                  //   Scanner sc = new Scanner ( System.in);
+                    //System.out.println("enter your age ...");
+                      //  int a = sc.nextInt();
+
+                    //if (a < 18){
+                      // String str1 = "not elligle";
+                    // System.out.println("STATUS : " + str1);
+                      
+                    //}else {
+                      
+                      // String str2 = "elligle";
+
+                        //System.out.println("STATUS :" + str2);
+                     //}
+                   //}
+     //     }         
+     
+
+                         
+                      markGrade();
+   
+                     }             
+                      
+                      static void markGrade(){  
+                         Scanner sc = new Scanner(System.in);
+                        System.out.println("enter your marks...");
+                      int marks = sc.nextInt();
+                         
+                        if ( marks >= 91){
+                           String  grade1 = "AA";
+                       System.out.println( "GRADE : " + grade1);
+                     
+                        }else if ( marks >= 81){
+                           String grade2 = "AB";
+                         System.out.println("GRADE : " + grade2);
+           
+                         }else if ( marks >= 71){
+                            String grade3 = "BB";
+                         System.out.println("GRADE: " + grade3);
+                     
+              
+                        }else if ( marks >= 61){
+                          String  grade4 = "BC";
+                         System.out.println("GRADE : " + grade4);
+                       
+                         }else if ( marks >= 51){
+                             String grade5 = "CD";
+                          System.out.println("GRADE: " + grade5);
+                       
+                        }else if (marks >= 41){
+                            String  grade6 = "DD";
+                         System.out.println("GRADE: " + grade6);
+               
+                         }else if (marks <= 40){
+                            String  grade7 = "fail";
+                         System.out.println("GRADE: " + grade7);
+  
+                 
+                        }else if (marks < 0){
+                           String  grade8 = "poor";
+                        System.out.println("GRADE: " + grade8);
+                      }
+                 }
+              }     
+                   
+                     
+                 
+              
+                     
+
+
+
+
+                               
+
+
+
+
+
+
+
